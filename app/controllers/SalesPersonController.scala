a8b74d5c86669893d014d0ded143cadeb9549b35
Bert Huygens <bert.huygens@gmail.com>
2012-11-21 10:24:16 +0100

    edit delete list salespersons

--- app/controllers/SalesPersonController.scala
+++ app/controllers/SalesPersonController.scala
@@ -9,8 +9,12 @@ import play.api.data.format.Formats.longFormat
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import views.html
-
+import models._
 import anorm._
+import anorm.SqlParser._
+import play.api.Play.current
+import play.api.db.DB
+import models.SalesPerson
 
 object SalesPersonController extends Controller {
   
@@ -25,12 +29,38 @@ object SalesPersonController extends Controller {
   (SalesPerson.unapply)
   )
   
+  val Home = Redirect(routes.SalesPersonController.list(0, 2, ""))
+  
   def create = Action {
     Ok(html.salespersons.createForm(salespersonForm))
   }
   
-  def list = Action {
-   Ok(html.salespersons.salespersonsOverview(SalesPerson.all));
+  def edit(id: Long) = Action {
+    SalesPerson.findById(id).map { salesperson =>
+      Ok(html.salespersons.editForm(id, salespersonForm.fill(salesperson)))
+    }.getOrElse(NotFound)
+  }
+  
+  def update(id: Long) = Action { implicit request =>
+    salespersonForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.salespersons.editForm(id, formWithErrors)),
+      salesperson => {
+        SalesPerson.update(id, salesperson)
+        Home.flashing("success" -> "Person %s has been updated".format(salesperson.name))
+      }
+    )
+  }
+  
+  def list(page: Int, orderBy: Int, filter: String) = Action { implicit request =>
+    Ok(html.salespersons.salespersonsOverview(
+      SalesPerson.list(page = page, orderBy = orderBy, filter = ("%"+filter+"%")),
+      orderBy, filter
+    ))
+  }
+  
+  def delete(id: Long) = Action {
+    SalesPerson.delete(id)
+    Home
   }
   
   def save = Action { implicit request =>
@@ -38,7 +68,7 @@ object SalesPersonController extends Controller {
       formWithErrors => BadRequest(html.salespersons.createForm(formWithErrors)),
       salesPerson => {
         SalesPerson.insert(salesPerson)
-        Redirect(routes.SalesPersonController.list)
+        Home
       }
     )
     
--- app/models/SalesPerson.scala
+++ app/models/SalesPerson.scala
@@ -21,9 +21,73 @@ object SalesPerson {
       case id~name~firstName~mobile => SalesPerson(id, name, firstName, mobile)
     }
   }
+  
+  def list(page: Int = 0, pageSize: Int = 10, orderBy: Int = 1, filter: String = "%"): Page[SalesPerson] = {
+    
+    val offest = pageSize * page
+    
+    DB.withConnection { implicit connection =>
+      
+      val salespersons = SQL(
+        """
+          select * from salespersons 
+          where salespersons.name like {filter}
+          order by {orderBy} nulls last
+          limit {pageSize} offset {offset}
+        """
+      ).on(
+        'pageSize -> pageSize, 
+        'offset -> offest,
+        'filter -> filter,
+        'orderBy -> orderBy
+      ).as(SalesPerson.salesPersonParser *)
 
+      val totalRows = SQL(
+        """
+          select count(*) from salespersons 
+         where salespersons.name like {filter}
+        """
+      ).on(
+        'filter -> filter
+      ).as(scalar[Long].single)
+
+      Page(salespersons, page, offest, totalRows)
+      
+    }
+    
+  }
+  
   def all(): List[SalesPerson] = DB.withConnection {
     implicit c => SQL("select * from salespersons").as(salesPersonParser *)
+  }
+  
+  def findById(id : Long) : Option[SalesPerson] = {
+    DB.withConnection { implicit connection =>
+      SQL("select * from salespersons where id = {id}").on('id -> id).as(salesPersonParser.singleOpt)
+    }
+  }
+  
+  def update(id: Long, salesPeron : SalesPerson) = {
+	  DB.withConnection { implicit connection =>
+      SQL(
+        """
+          update salespersons
+          set name = {name}, firstName = {firstName}, mobile = {mobile}
+          where id = {id}
+        """
+      ).on(
+        'id -> id,
+        'name -> salesPeron.name,
+        'introduced -> salesPeron.firstName,
+        'discontinued -> salesPeron.mobile
+      ).executeUpdate()
+    }
+  }
+  
+  def delete(id: Long) = {
+    DB.withConnection { implicit connection =>
+      SQL("delete from salespersons where id = {id}").on('id -> id).executeUpdate()
+    }
   }
   
   def insert(salesPerson : SalesPerson) = DB.withConnection {
